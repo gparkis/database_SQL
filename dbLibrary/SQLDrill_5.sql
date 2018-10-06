f29aaa23402630a079745c3560d34daa72fe91f5
gparkis <gparkis@gmail.com>
2018-10-06 19:02:59 -0400

    Update SQLDrill_5.sql

--- dbLibrary/SQLDrill_5.sql
+++ dbLibrary/SQLDrill_5.sql
@@ -1,15 +1,13 @@
 SELECT * FROM tblLibrary_Branch
 SELECT * FROM tblBook_Loans
 
---this is a way more efficent way to determine the number of books on loan for each library--
+
 SELECT BranchName, COUNT(BookId) AS #BooksLoaned
 FROM tblBook_Loans INNER JOIN tblLibrary_Branch
 ON tblBook_Loans.BranchId = tblLibrary_Branch.BranchId
 GROUP BY BranchName
 HAVING COUNT(BookId)>1
 
-
---these COUNT various specific parameters to determine the number of books on loan from a specific branch--
 
 SELECT COUNT(BookId)
 FROM tblBook_Loans INNER JOIN tblLibrary_Branch
